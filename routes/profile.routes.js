db3461aaca218ec2cef13d172b84627811d7612a
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-02-15 19:18:06 +0100

    iteration3

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -5,6 +5,16 @@ const router = express.Router();
 
 // GET "/profile"
 router.get("/", (req, res, next) => {
+
+
+  if (req.session.activeUser === undefined) {
+    res.redirect("/auth/login");
+  } else {
+    next()
+  }
+
     res.render("profile/perfil.hbs")
 })
+
+
 module.exports = router;
